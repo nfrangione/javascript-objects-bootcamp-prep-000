b61565e4dbe91745262b4f2d45903cb9d1fe46de
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-28 04:31:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({PnBRock: "Selfish"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle});
+  return Object.assign({}, playlist, { [artistName]: songTitle });
 }
